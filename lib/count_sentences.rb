5be5aefc5b2dd94710f317e611d41043f8f36456
Jake G <jake098877@gmail.com>
2020-05-05 11:28:09 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?"!"
   end
 
   def count_sentences
+  array = []
+  array <<  self.split(".")
+  binding.pry
+  array << self.split("!")
+  array << self.split("?")
+   count = 0
+    array.each do |i|
+    count += 1
+    end
+    count
 
+
+
+    puts "Count is: "
   end
 end
